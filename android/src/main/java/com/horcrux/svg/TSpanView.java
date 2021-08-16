2a1bc2b758a4bc872008f552c93838c65dbdba33
Ashwin <ashwin@springr.in>
2021-08-16 19:36:53 +0530

    constant warning

--- android/src/main/java/com/horcrux/svg/TSpanView.java
+++ android/src/main/java/com/horcrux/svg/TSpanView.java
@@ -165,7 +165,6 @@ class TSpanView extends TextView {
     }
 
     @SuppressWarnings("deprecation")
-    @SuppressLint("WrongConstant")
     private StaticLayout getStaticLayout(TextPaint tp, Layout.Alignment align, boolean includeFontPadding, SpannableString text, int width) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
             return new StaticLayout(
@@ -177,6 +176,7 @@ class TSpanView extends TextView {
                     0.f,
                     includeFontPadding);
         } else {
+            @SuppressLint("WrongConstant")
             return StaticLayout.Builder.obtain(text, 0, text.length(), tp, width)
                     .setAlignment(align)
                     .setLineSpacing(0.f, 1.f)
